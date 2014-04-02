b8cc990a7bf796453a258ff3c12e77fe0ef34ce3
tobiaskneubuehler <tobias@joinbox.com>
2014-04-02 12:21:45 +0200

    index.js added semicol

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 
     module.exports = require('./lib/Service');
-    module.exports.DefaultController = require('./lib/DefaultController')
+    module.exports.DefaultController = require('./lib/DefaultController');
