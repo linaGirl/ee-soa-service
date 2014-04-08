64b962aacdee92d57b537fa5f0594212ede987f0
tobiaskneubuehler <tobias@joinbox.com>
2014-04-08 20:31:52 +0200

    service abstraction

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 
-    module.exports = require('./lib/Service');
-    module.exports.DefaultController = require('./lib/DefaultController');
+    module.exports                      = require('./lib/Service');
+    module.exports.DefaultController    = require('./lib/DefaultController');
+    module.exports.DefaultORMController = require('./lib/DefaultORMController');
