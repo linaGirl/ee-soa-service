900a04d5d999d3a1d86a85eb75500ed9f042e9d9
tobiaskneubuehler <tobias@joinbox.com>
2014-05-20 14:16:12 +0200

    defaultservice in index && Controller? in name

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
     module.exports                      = require('./lib/Service');
+    module.exports.DefaultService       = require('./lib/DefaultService');
     module.exports.DefaultController    = require('./lib/DefaultController');
     module.exports.DefaultORMController = require('./lib/DefaultORMController');
