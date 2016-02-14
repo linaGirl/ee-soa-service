5d91fd611c3b72fb98f76a7e12aa6d171350d755
Michael van der Weg <michael@joinbox.com>
2016-02-15 00:13:31 +0100

    package.json
    added dev depdendencies for testing the legacy request brudge

---
+++ src/LegacyRequestBridge.js
@@ -0,0 +1,22 @@
+(function() {
+	'use strict';
+
+
+
+	module.exports = class LegacyRequestBridge {
+
+
+
+		/**
+		 * convert old style soa requests and
+		 * responses to new style distributed 
+		 * requests
+		 *
+		 * @param {object} request
+		 * @param {object} response
+		 */
+		convert(request, response) {
+			
+		}
+	};
+})();
